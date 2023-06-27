bf45de78f8ecba2c5eb2d31ab9ce4c24f490c609
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-06-26 19:10:06 -0600

    basic file server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use axum::{routing::get, Router};
+use axum::Router;
+use tower_http::services::ServeDir;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello there!" }));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "photo_manager_server=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().nest_service("/images", ServeDir::new("assets"));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
