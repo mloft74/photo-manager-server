258c9c0b9db10c90c6134df8fc7cd50364410597
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-07-06 23:24:59 -0600

    Update ActionProvider

--- src/domain/actions.rs
+++ src/domain/actions.rs
@@ -1,8 +1,11 @@
-use crate::domain::actions::images::ImageSaver;
+use crate::domain::actions::images::{ImageGetter, ImageSaver};
 
 pub mod images;
 
 pub trait ActionProvider {
+    type ImageGetterImpl: ImageGetter;
+    fn get_image_getter(&self) -> Self::ImageGetterImpl;
+
     type ImageSaverImpl: ImageSaver;
     fn get_image_saver(&self) -> Self::ImageSaverImpl;
 }
--- src/persistence/persistence_manager.rs
+++ src/persistence/persistence_manager.rs
@@ -10,11 +10,20 @@ impl PersistenceManager {
     pub fn new(db_conn: DatabaseConnection) -> Self {
         Self { db_conn }
     }
+
+    fn new_image_manager(&self) -> ImageManager {
+        ImageManager::new(self.db_conn.clone())
+    }
 }
 
 impl ActionProvider for PersistenceManager {
+    type ImageGetterImpl = ImageManager;
+    fn get_image_getter(&self) -> Self::ImageGetterImpl {
+        self.new_image_manager()
+    }
+
     type ImageSaverImpl = ImageManager;
     fn get_image_saver(&self) -> Self::ImageSaverImpl {
-        ImageManager::new(self.db_conn.clone())
+        self.new_image_manager()
     }
 }
