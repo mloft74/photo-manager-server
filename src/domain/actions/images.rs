3627d7ada5f569f25212fbcfd7b0e55587d6559b
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-07-06 23:22:23 -0600

    Implement ImageGetter

--- src/domain/actions/images.rs
+++ src/domain/actions/images.rs
@@ -6,3 +6,8 @@ use crate::domain::models::Image;
 pub trait ImageSaver: Clone + Send + Sync {
     async fn save_image(&self, image: &Image) -> Result<(), Box<dyn std::error::Error>>;
 }
+
+#[async_trait]
+pub trait ImageGetter: Clone + Send + Sync {
+    async fn get_image(&self, file_name: &str) -> Result<Image, Box<dyn std::error::Error>>;
+}
--- src/persistence/image_manager.rs
+++ src/persistence/image_manager.rs
@@ -1,8 +1,11 @@
 use async_trait::async_trait;
-use sea_orm::{DatabaseConnection, EntityTrait};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
 use crate::{
-    domain::{actions::images::ImageSaver, models::Image},
+    domain::{
+        actions::images::{ImageGetter, ImageSaver},
+        models::Image,
+    },
     persistence::entities::{images, prelude::Images},
 };
 
@@ -11,14 +14,24 @@ pub struct ImageManager {
     db_conn: DatabaseConnection,
 }
 
-// Hide this behind Traits and use generics to pass it around.
 impl ImageManager {
     pub fn new(db_conn: DatabaseConnection) -> Self {
         Self { db_conn }
     }
+}
 
-    pub async fn get_image() -> Result<Image, Box<dyn std::error::Error>> {
-        todo!()
+#[async_trait]
+impl ImageGetter for ImageManager {
+    async fn get_image(&self, file_name: &str) -> Result<Image, Box<dyn std::error::Error>> {
+        let model = Images::find()
+            .filter(images::Column::FileName.eq(file_name))
+            .one(&self.db_conn)
+            .await?
+            .ok_or_else(|| format!("Could not find image with file name {}", file_name))?;
+
+        Ok(Image {
+            file_name: model.file_name,
+        })
     }
 }
 
@@ -29,7 +42,6 @@ impl ImageSaver for ImageManager {
             file_name: sea_orm::ActiveValue::Set(image.file_name.clone()),
             ..Default::default()
         };
-
         Images::insert(model).exec(&self.db_conn).await?;
 
         Ok(())
