124f2bd159c3f4d6d5e54a76620a2a318ade1027
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-07-08 10:28:48 -0600

    Add distinct errors for

--- src/api/routing/image/upload.rs
+++ src/api/routing/image/upload.rs
@@ -7,6 +7,8 @@ use axum::{
 };
 use futures::{Stream, TryStreamExt};
 use hyper::{body::Bytes, StatusCode};
+use serde::Serialize;
+use serde_json::json;
 use tokio::{fs::File, io::BufWriter};
 use tokio_util::io::StreamReader;
 use tower_http::limit::RequestBodyLimitLayer;
@@ -26,47 +28,77 @@ pub fn make_upload_router<T: ImageSaver + 'static>(image_saver: T) -> Router {
         .with_state(image_saver)
 }
 
+#[derive(Serialize)]
+enum UploadImageError {
+    FileNameNotPresent,
+    GeneralError(String),
+}
+
+impl UploadImageError {
+    fn to_json_string(&self) -> String {
+        serde_json::to_string(self).unwrap_or_else(|e| {
+            json!({
+                "error": "jsonConverionFailed",
+                "message": e.to_string(),
+            })
+            .to_string()
+        })
+    }
+}
+
 // Handler that accepts a multipart form upload and streams each field to a file.
 async fn upload_image<T: ImageSaver>(
-    state: State<T>,
+    saver: State<T>,
     mut multipart: Multipart,
 ) -> Result<(), (StatusCode, String)> {
     let mut outer_file_name = None;
+    let mut count = 0;
     while let Ok(Some(field)) = multipart.next_field().await {
+        tracing::debug!("iteration: {}", count);
+        count += 1;
         let file_name = if let Some(file_name) = field.file_name() {
             file_name.to_owned()
         } else {
             continue;
         };
 
-        stream_to_file(&file_name, field).await?;
+        stream_to_file(&file_name, field)
+            .await
+            .map_err(|(s, e)| (s, e.to_json_string()))?;
         if outer_file_name.is_some() {
             continue;
         }
         outer_file_name = Some(file_name)
     }
 
-    let file_name = outer_file_name.ok_or((
-        StatusCode::BAD_REQUEST,
-        "File name not found in request".to_string(),
-    ))?;
+    let file_name = outer_file_name.ok_or_else(|| {
+        (
+            StatusCode::BAD_REQUEST,
+            UploadImageError::FileNameNotPresent.to_json_string(),
+        )
+    })?;
 
-    state
-        .save_image(&Image { file_name })
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    saver.save_image(&Image { file_name }).await.map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            UploadImageError::GeneralError(e.to_string()).to_json_string(),
+        )
+    })?;
 
     Ok(())
 }
 
 // Save a `Stream` to a file
-async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
+async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, UploadImageError)>
 where
     S: Stream<Item = Result<Bytes, E>>,
     E: Into<BoxError>,
 {
     if !path_is_valid(path) {
-        return Err((StatusCode::BAD_REQUEST, "Invalid path".to_string()));
+        return Err((
+            StatusCode::BAD_REQUEST,
+            UploadImageError::GeneralError("Invalid path".to_string()),
+        ));
     }
 
     async {
@@ -85,7 +117,12 @@ where
         Ok::<_, io::Error>(())
     }
     .await
-    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+    .map_err(|err| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            UploadImageError::GeneralError(err.to_string()),
+        )
+    })
 }
 
 // to prevent directory traversal attacks we ensure the path consists of exactly one normal
