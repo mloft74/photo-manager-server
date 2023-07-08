162a3b7a0da47d8eaf44b0ba8a8220dc63426136
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-07-08 17:39:58 -0600

    remove column default

---
+++ src/persistence/migrator/m20230708_231248_remove_width_and_height_defaults.rs
@@ -0,0 +1,21 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared("ALTER TABLE \"images\" ALTER COLUMN \"width\" DROP DEFAULT, ALTER COLUMN \"height\" DROP DEFAULT;").await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared("ALTER TABLE \"images\" ALTER COLUMN \"width\" SET DEFAULT 0, ALTER COLUMN \"height\" SET DEFAULT 0;").await?;
+
+        Ok(())
+    }
+}
--- src/persistence/migrator/mod.rs
+++ src/persistence/migrator/mod.rs
@@ -4,6 +4,7 @@ use sea_orm_migration::prelude::*;
 
 mod m20230707_014242_create_images_table;
 mod m20230708_223818_add_image_size_columns;
+mod m20230708_231248_remove_width_and_height_defaults;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20230707_014242_create_images_table::Migration),
             Box::new(m20230708_223818_add_image_size_columns::Migration),
+            Box::new(m20230708_231248_remove_width_and_height_defaults::Migration),
         ]
     }
 }
