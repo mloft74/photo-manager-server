65b56555972ee68e05ba52513de6eb71f02cfd77
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2024-02-19 16:33:06 -0700

    Start writing scale image function

--- src/api/image_dimensions.rs
+++ src/api/image_dimensions.rs
@@ -1,7 +1,10 @@
-use image::io::Reader as ImageReader;
+use image::{
+    imageops::{resize, FilterType},
+    io::Reader as ImageReader,
+};
 use serde::Serialize;
 
-use crate::api::IMAGES_DIR;
+use crate::{api::IMAGES_DIR, domain::models::Image};
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -20,3 +23,28 @@ pub fn fetch_image_dimensions(file_name: &str) -> Result<(u32, u32), FetchImageD
 
     Ok(dim)
 }
+
+pub enum ScaleImageError {
+    ErrorOpeningImage(String),
+    UnknownFormat(String),
+    ImageError(String),
+}
+
+pub fn scale_image(image: &Image) -> Result<(), ScaleImageError> {
+    let path = std::path::Path::new(IMAGES_DIR).join(&image.file_name);
+    let fs_image =
+        ImageReader::open(path).map_err(|e| ScaleImageError::ErrorOpeningImage(e.to_string()))?;
+    let format = fs_image
+        .format()
+        .ok_or_else(|| ScaleImageError::UnknownFormat(image.file_name.clone()))?;
+
+    let decoded = fs_image
+        .decode()
+        .map_err(|e| ScaleImageError::ImageError(e.to_string()))?;
+
+    let resized = resize(&decoded, 1920, 1080, FilterType::Lanczos3);
+
+    Ok(())
+}
+
+pub fn scale_images(images: &[Image]) {}
