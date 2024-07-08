369267a50c66a3e3e56dcbc1e961516685cbf209
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2024-07-08 07:41:00 -0600

    Move socket code to new module

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,11 @@
-use std::{
-    fmt::Debug,
-    io::{self, BufRead, BufReader, Write},
-    net::{Shutdown, SocketAddr, TcpListener, TcpStream},
-    sync::mpsc::{self, Receiver, TryRecvError},
-    thread::{self, JoinHandle},
-    time::Duration,
-};
+use std::{fmt::Debug, sync::mpsc};
 
 use dotenvy::dotenv;
-use tracing::{debug, error, warn};
 
 mod api;
 mod domain;
 mod persistence;
+mod rtc;
 mod server_tracing;
 mod state;
 
@@ -25,13 +18,9 @@ pub async fn run() {
 
     let api_router = api::make_api_router(&persistence_mngr).await;
 
-    // TODO: move thread code into a new location
-
-    // TODO: manually test this code
-
     let (send_stop_listen, recv_stop_listen) = mpsc::channel();
 
-    let listen_handler = listener_thread(recv_stop_listen);
+    let listen_handler = rtc::listener_thread(recv_stop_listen);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
@@ -45,152 +34,6 @@ pub async fn run() {
     listen_handler
         .join()
         .expect("Listen thread should not panic");
-}
-
-fn listener_thread(recv_stop_listen: Receiver<()>) -> JoinHandle<()> {
-    thread::spawn(move || {
-        let listener = TcpListener::bind("0.0.0.0:4000").expect("TcpListener should be valid");
-        listener
-            .set_nonblocking(true)
-            .expect("Should be able to set listener as non-blocking");
-        let mut stream_threads = Vec::new();
-        debug!("now listening");
-        loop {
-            let should_stop = recv_stop_listen.try_recv();
-            match should_stop {
-                Err(TryRecvError::Empty) => (),
-                Err(TryRecvError::Disconnected) => {
-                    warn!("recv_stop_listen disconnected, stopping thread");
-                    break;
-                }
-                Ok(_) => {
-                    debug!("recv_stop_listen received stop signal, stopping thread");
-                    break;
-                }
-            }
-            let res = listener.accept();
-            match res {
-                Ok((stream, addr)) => {
-                    debug!("established connection with {addr}");
-                    let (send_stop, recv_stop) = mpsc::channel();
-                    let thread = stream_thread(recv_stop, stream, addr);
-                    stream_threads.push((send_stop, addr, thread));
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => (),
-                Err(e) => {
-                    error!("encountered error accepting tcp connection: {e}");
-                }
-            }
-
-            let mut finished_idxs = Vec::new();
-            for (idx, (_, _, thread)) in stream_threads.iter().enumerate() {
-                if thread.is_finished() {
-                    finished_idxs.push(idx);
-                }
-            }
-            for idx in finished_idxs {
-                let (_, addr, thread) = stream_threads.remove(idx);
-                debug!("detected that {addr} thread finished, joining");
-                let res = thread.join();
-                if let Err(err) = res {
-                    error!("peer thread {addr} failed to join: {err:?}");
-                } else {
-                    debug!("finished joining {addr} thread");
-                }
-            }
-
-            thread::sleep(Duration::from_millis(100));
-        }
-
-        let (send_stops, threads) =
-            stream_threads
-                .into_iter()
-                .fold((Vec::new(), Vec::new()), |mut acc, element| {
-                    let (send_stop, addr, thread) = element;
-                    acc.0.push((send_stop, addr));
-                    acc.1.push((thread, addr));
-                    acc
-                });
-        for (send_stop, addr) in send_stops {
-            let res = send_stop.send(());
-            if let Err(err) = res {
-                error!("failed to send stop signal to thread for {addr}: {err}");
-            }
-        }
-        for (thread, addr) in threads {
-            let res = thread.join();
-            if let Err(err) = res {
-                error!("failed to join thread for {addr}: {err:?}");
-            }
-        }
-    })
-}
-
-fn stream_thread(recv_stop: Receiver<()>, stream: TcpStream, addr: SocketAddr) -> JoinHandle<()> {
-    thread::spawn(move || {
-        stream
-            .set_nonblocking(false)
-            .expect("Should be able to set stream as blocking");
-        let mut reader = BufReader::new(stream);
-        debug!("[{addr}] now handling stream");
-        'thread: loop {
-            let should_stop = recv_stop.try_recv();
-            match should_stop {
-                Err(TryRecvError::Empty) => (),
-                Err(TryRecvError::Disconnected) => {
-                    warn!("[{addr}] recv_stop disconnected, stopping thread");
-                    break;
-                }
-                Ok(_) => {
-                    debug!("[{addr}] recv_stop received stop signal, stopping thread");
-                    break;
-                }
-            }
-
-            let mut string = String::new();
-            {
-                let res = reader.read_line(&mut string);
-                match res {
-                    Ok(0) => {
-                        debug!("[{addr}] got no bytes | string: {string}");
-                    }
-                    Ok(x) => {
-                        debug!("[{addr}] got {x} bytes | string: {string}");
-                    }
-                    Err(e) => {
-                        error!("[{addr}] encountered error reading from tcp connection: {e}");
-                        break 'thread;
-                    }
-                }
-            }
-
-            if !string.is_empty() {
-                debug!("[{addr}] got msg: {string}");
-                let msg = format!("[{addr}] sending prompted message to peer");
-                let res = reader.get_ref().write_all(msg.as_bytes());
-                if let Err(err) = res {
-                    error!("[{addr}] error sending message to peer: {err}");
-                    break;
-                }
-            } else {
-                debug!("[{addr}] got no msg, sending to peer anyway");
-                let msg = format!("[{addr}] sending unprompted message to peer");
-                let res = reader.get_ref().write_all(msg.as_bytes());
-                if let Err(err) = res {
-                    error!("[{addr}] error sending message to peer: {err}");
-                    break;
-                }
-            }
-
-            thread::sleep(Duration::from_millis(100));
-        }
-
-        debug!("[{addr}] shutting down connection");
-        let res = reader.get_ref().shutdown(Shutdown::Both);
-        if let Err(err) = res {
-            error!("[{addr}] could not close peer connection because error: {err}");
-        }
-    })
 }
 
 trait LazyExpect {
---
+++ src/rtc.rs
@@ -0,0 +1,160 @@
+use std::{
+    io::{self, BufRead, BufReader, Write},
+    net::{Shutdown, SocketAddr, TcpListener, TcpStream},
+    sync::mpsc::{self, Receiver, TryRecvError},
+    thread::{self, JoinHandle},
+    time::Duration,
+};
+
+use tracing::{debug, error, warn};
+
+pub fn listener_thread(recv_stop_listen: Receiver<()>) -> JoinHandle<()> {
+    thread::spawn(move || {
+        let listener = TcpListener::bind("0.0.0.0:4000").expect("TcpListener should be valid");
+        listener
+            .set_nonblocking(true)
+            .expect("Should be able to set listener as non-blocking");
+        debug!("now listening");
+
+        let mut stream_threads = Vec::new();
+        loop {
+            let should_stop = recv_stop_listen.try_recv();
+            match should_stop {
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => {
+                    warn!("recv_stop_listen disconnected, stopping thread");
+                    break;
+                }
+                Ok(_) => {
+                    debug!("recv_stop_listen received stop signal, stopping thread");
+                    break;
+                }
+            }
+
+            let res = listener.accept();
+            match res {
+                Ok((stream, addr)) => {
+                    debug!("established connection with {addr}");
+                    let (send_stop, recv_stop) = mpsc::channel();
+                    let thread = stream_thread(recv_stop, stream, addr);
+                    stream_threads.push((send_stop, addr, thread));
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => (),
+                Err(e) => {
+                    error!("encountered error accepting tcp connection: {e}");
+                }
+            }
+
+            // This section is written with mutating loops instead of list transforms
+            // because we need to remove threads from the thread list as we go.
+            let mut finished_idxs = Vec::new();
+            for (idx, (_, _, thread)) in stream_threads.iter().enumerate() {
+                if thread.is_finished() {
+                    finished_idxs.push(idx);
+                }
+            }
+            for idx in finished_idxs {
+                let (_, addr, thread) = stream_threads.remove(idx);
+                debug!("detected that {addr} thread finished, joining");
+                let res = thread.join();
+                if let Err(err) = res {
+                    error!("peer thread {addr} failed to join: {err:?}");
+                } else {
+                    debug!("finished joining {addr} thread");
+                }
+            }
+
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        let (send_stops, threads) =
+            stream_threads
+                .into_iter()
+                .fold((Vec::new(), Vec::new()), |mut acc, element| {
+                    let (send_stop, addr, thread) = element;
+                    acc.0.push((send_stop, addr));
+                    acc.1.push((thread, addr));
+                    acc
+                });
+        for (send_stop, addr) in send_stops {
+            let res = send_stop.send(());
+            if let Err(err) = res {
+                error!("failed to send stop signal to thread for {addr}: {err}");
+            }
+        }
+        for (thread, addr) in threads {
+            let res = thread.join();
+            if let Err(err) = res {
+                error!("failed to join thread for {addr}: {err:?}");
+            }
+        }
+    })
+}
+
+fn stream_thread(recv_stop: Receiver<()>, stream: TcpStream, addr: SocketAddr) -> JoinHandle<()> {
+    thread::spawn(move || {
+        stream
+            .set_nonblocking(false)
+            .expect("Should be able to set stream as blocking");
+        let mut reader = BufReader::new(stream);
+        debug!("[{addr}] now handling stream");
+
+        loop {
+            let should_stop = recv_stop.try_recv();
+            match should_stop {
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => {
+                    warn!("[{addr}] recv_stop disconnected, stopping thread");
+                    break;
+                }
+                Ok(_) => {
+                    debug!("[{addr}] recv_stop received stop signal, stopping thread");
+                    break;
+                }
+            }
+
+            let mut string = String::new();
+            {
+                let res = reader.read_line(&mut string);
+                match res {
+                    Ok(0) => {
+                        debug!("[{addr}] got no bytes | string: {string}");
+                    }
+                    Ok(x) => {
+                        debug!("[{addr}] got {x} bytes | string: {string}");
+                    }
+                    Err(e) => {
+                        error!("[{addr}] encountered error reading from tcp connection: {e}");
+                        break;
+                    }
+                }
+            }
+
+            if !string.is_empty() {
+                debug!("[{addr}] got msg: {string}");
+                let msg = format!("[{addr}] sending prompted message to peer");
+                let res = reader.get_ref().write_all(msg.as_bytes());
+                if let Err(err) = res {
+                    error!("[{addr}] error sending message to peer: {err}");
+                    break;
+                }
+            } else {
+                debug!("[{addr}] got no msg, sending to peer anyway");
+                let msg = format!("[{addr}] sending unprompted message to peer");
+                let res = reader.get_ref().write_all(msg.as_bytes());
+                if let Err(err) = res {
+                    error!("[{addr}] error sending message to peer: {err}");
+                    break;
+                }
+            }
+
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        debug!("[{addr}] shutting down connection");
+        let res = reader.get_ref().shutdown(Shutdown::Both);
+        if let Err(err) = res {
+            error!("[{addr}] could not close peer connection because error: {err}");
+        }
+    })
+}
