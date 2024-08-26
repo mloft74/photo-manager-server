823f846cd85ceec6d2c0aec570bcec74300bcd73
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2024-08-26 14:46:07 -0600

    Wire up line up route

--- src/api/routing/image/line_up.rs
+++ src/api/routing/image/line_up.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Json, Router};
 use serde::Serialize;
 
-use crate::domain::screensaver::Screensaver;
+use crate::{api::routing::image::ImageResponse, domain::screensaver::Screensaver};
 
 pub fn make_line_up_router(
     screensaver_mngr: impl 'static + Clone + Send + Sync + Screensaver,
@@ -11,8 +11,15 @@ pub fn make_line_up_router(
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-struct LineUpResponse {}
+struct LineUpResponse {
+    images: Vec<ImageResponse>,
+    current_idx: Option<usize>,
+}
 
 fn line_up(screensaver: impl Screensaver) -> Json<LineUpResponse> {
-    todo!();
+    let line_up = screensaver.get_line_up();
+    Json(LineUpResponse {
+        images: line_up.images.into_iter().map(|i| i.into()).collect(),
+        current_idx: line_up.current_idx,
+    })
 }
